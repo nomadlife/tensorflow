1bc4baf12fe86611b800efd21d9905bedbceace1
nomadlife <joonwoo3816@gmail.com>
2017-07-25 12:10:05 +0900

    ex2_4

--- ex1-1.py
+++ ex1-1.py
@@ -1,5 +1,18 @@
+# tf.contrib.learn
+
 import numpy as np
 #Numpy is often used to load, manipulate and preprocess data.
 import tensorflow as tf
 
 feature = [tf.contrib.layers.real_valued_column("x", dimention=1)]
+estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
+
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
+input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x_train}, y_train,
+                                              batch_size=4,
+                                              num_epochs=1000)
+eval_input_fn = tf.contrib.learn.io.numpy_input_fn(
+    {"x":x_eval}, y_eval, batch_size=4, num_epochs=1000)
